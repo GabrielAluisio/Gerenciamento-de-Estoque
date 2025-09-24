d565e51afafcf3ed189e743a4a685339a43f1228
GabrielDiniz <aluisiogabrieldiniz@gmail.com>
2025-09-24 02:38:36 -0300

    Programa principal escolha 1 feita

--- Beck-End/programa.py
+++ Beck-End/programa.py
@@ -12,7 +12,21 @@ _________________________________
 Escolha uma opção: ''')
 
 if pergunta == '1':
-
+    nome = input('Nome: ')
+    total_no_estoque = input('Total no estoque: ')
+    valor = input('Valor (unidade): ')
     
+    print('-----------')
+    app.mostrar_tabela("Categorias")
+    print('-----------')
+    categoria= input('Escolha uma opção:')
+
+
+    app.adicionar_dados('Produtos', (nome, total_no_estoque, valor, categoria))
+
     app.mostrar_tabela('Produtos')
+
+if pergunta == '2':
+
+
 
